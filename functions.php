c5849aa33069128e851c0b46adda049967aabdd7
MarinaMich <mar7995@yandex.ru>
2021-01-29 15:03:22 +0300

    Все функции из register.php вынесены в functions.php

---
+++ functions.php
@@ -0,0 +1,63 @@
+<?php
+/* поиск пользователя в БД по эл. адресу
+	string - $email
+	Return value: array 
+*/ 
+function get_user_by_email($email,  $pdo) {
+	$guery = "SELECT * FROM users WHERE email=:email";
+	$stmt = $pdo->prepare($guery);
+	$stmt->execute(['email' => $email]);
+	$task = $stmt->fetch()['email'];
+	return $task;	
+};
+
+/* добавить пользователя в БД
+	string -$email
+	string - $password
+	Return value: int (user_id)
+*/
+function add_user($email, $password, $pdo) {
+	$query = "INSERT INTO users (email, password) VALUES (:email, :password)";
+	$stmt = $pdo->prepare($query);
+	$stmt ->execute([
+		'email' => $email,
+		'password' => $password
+	]);
+	//вернет ID последнего созданного пользователя
+	//первый вариант
+	$id = $pdo->lastInsertId();
+	//второй вариант
+	//$user = $stmt->fetch(PDO::FETCH_ASSOC);
+	// return $user['id'];
+};
+
+/* подготовка флеш сообщения
+	string - $key
+	string - $message (значение, текст сообщения)
+	Return value: null
+*/
+function set_flash_message($key, $message) {
+	$_SESSION[$key] = $message;
+
+};
+
+/* вывести флеш сообщение
+	string - $key
+	Return value: null
+*/	
+function display_flash_message($key) {
+	if (isset ($_SESSION [$key])) {
+		
+		echo '<div class="alert alert-'. $key .' text-dark" role="alert">'. $_SESSION[$key] .'</div>';
+		unset($_SESSION[$key]); 
+	}
+};
+
+/* перенаправить на другую страницу
+	string - $path
+	Return value: null
+*/
+function redirect_to($path) {
+	header("Location: ".$path);
+	exit();
+};	
